75c2e72046668c29a154bcc652379c98403611fc
Tharun Reddy <77614000+Reddytharun266@users.noreply.github.com>
2022-12-02 10:27:43 +0530

    Add files via upload

--- IDE.js
+++ IDE.js
@@ -1,32 +1,33 @@
-function allow_drop(ev) {
-		  ev.preventDefault();
-		}
+function allow_drop(ev) 
+{
+	ev.preventDefault();
+}
 
-		function drag(ev) {
-		  ev.dataTransfer.setData("text", ev.target.id);
-		}
+function drag(ev) 
+{
+	ev.dataTransfer.setData("text", ev.target.id);
+}
 
-		function drop(ev) {
-		  ev.preventDefault();
-		  var data = ev.dataTransfer.getData("text");
-		  ev.target.append(document.getElementById(data).cloneNode(true));
+function drop(ev) 
+{
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("text");
+	ev.target.append(document.getElementById(data).cloneNode(true));
 
-		  var data = document.getElementById(data).cloneNode(true);
+	var data = document.getElementById(data).cloneNode(true);
 
-		  document.getElementById("div3").append(data.outerHTML);
-		}
+	document.getElementById("div3").append(data.outerHTML);
+}
 
-
-		function save() 
-		{
-
-			var div1 = document.getElementById("div3");
-			var htmlContent = [div1.textContent];
-			var bl = new Blob(htmlContent, {type: "text/html;charset=utf-8"});
-			var a = document.createElement("a");
-			a.href = URL.createObjectURL(bl);
-			a.download = "draganddrop.html";
-			a.hidden = true;
-			document.body.appendChild(a);
-			a.click();
-		}
+function save() 
+{
+	var div1 = document.getElementById("div3");
+	var htmlContent = [div1.textContent];
+	var bl = new Blob(htmlContent, {type: "text/html;charset=utf-8"});
+	var a = document.createElement("a");
+	a.href = URL.createObjectURL(bl);
+	a.download = document.getElementById("file_name").value + ".html";
+	a.hidden = true;
+	document.body.appendChild(a);
+	a.click();
+}
